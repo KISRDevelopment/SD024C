c25c341839553b894d099b27bcfb7b2f5602b869
smerri <smerri@kisr.edu.kw>
2024-10-24 12:50:38 +0300

    initilized Django

---
+++ SD024C/electronic_diagnosis_test/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ElectronicDiagnosisTestConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'electronic_diagnosis_test'
