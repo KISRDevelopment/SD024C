7696a73cfcf63ea3bbdcadd3f3f559bc73af44c3
smerri <smerri@kisr.edu.kw>
2025-07-01 14:42:52 +0300

    setup layout

--- SD024C/electronic_diagnosis_test/urls.py
+++ SD024C/electronic_diagnosis_test/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    
+    path("about",views.about, name="about"),
+    path("help",views.help, name="help"),
+    path("contact",views.contact, name="contact"),
+    path("login",views.login, name="login"),
 ]
--- SD024C/electronic_diagnosis_test/views.py
+++ SD024C/electronic_diagnosis_test/views.py
@@ -4,3 +4,32 @@ from django.shortcuts import render
 
 def index(request):
     return render(request,"index.html")
+
+def about (request):
+    return render (request,"about.html")
+
+def help (request):
+    return render (request,"help.html")
+
+def contact (request):
+    return render (request,"contact.html")
+
+def login (request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+
+        user = auth.authenticate(username=username, password=password)
+
+        if user is not None:
+            auth.login(request, user)
+            if request.user.is_staff:
+                return HttpResponseRedirect("superusers")
+            else:
+                return HttpResponseRedirect("students")
+
+        else:
+            messages.info(request, 'Invalid Username or Password')
+            return HttpResponseRedirect("login")
+    else:
+        return render(request, "login.html")
