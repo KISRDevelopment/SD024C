6c697db9d90beae81869b5e2db15e4a2d469000a
smerri <smerri@kisr.edu.kw>
2025-08-14 08:56:00 +0300

    data processing for test5

---
+++ SD024C/electronic_diagnosis_test/data/primary/test5.py
@@ -0,0 +1,321 @@
+
+
+primary_test5_training_questions = [
+    {
+        "id": 1,
+        "text": "",
+        "options": [
+            {"label": "أ", "text": "أَمَدَ", "value": "أ"},
+            {"label": "ب", "text": "أحْمَدَ", "value": "ب"},
+            {"label": "ج", "text": "أَهْمَدَ", "value": "ج"},
+        ],
+        "correct": "ب"
+    },
+    {
+        "id": 2,
+        "text": "",
+        "options": [
+            {"label": "أ", "text": "يزرعُ", "value": "أ"},
+            {"label": "ب", "text": "يَزْرعو", "value": "ب"},
+            {"label": "ج", "text": "يازْرَعُ", "value": "ج"},
+        ],
+        "correct": "ج"
+    },
+    {
+        "id": 3,
+        "text": "",
+        "options": [
+            {"label": "أ", "text": "كَثيرتُنْ", "value": "أ"},
+            {"label": "ب", "text": "كَثْرةٌ", "value": "ب"},
+            {"label": "ج", "text": "كثيرةٌ", "value": "ج"},
+        ],
+        "correct": "ب"
+    },
+]
+
+primary_test5_main_questions = [
+    {   
+        "id": 1,
+        "text": "",
+        "options": [
+            {"label": "أ", "text": "أنْتُمُ", "value": "أ"},
+            {"label": "ب", "text": "أنتمو", "value": "ب"},
+            {"label": "ج", "text": "أنتموا", "value": "ج"},
+        ],
+        "correct": "ب"
+    },
+    {
+        "id": 2,
+        "text": "",
+        "options": [
+            {"label": "أ", "text": "كننّا", "value": "أ"},
+            {"label": "ب", "text": "كَأَننّا", "value": "ب"},
+            {"label": "ج", "text": " كَاأننّا", "value": "ج"},
+        ],
+        "correct": "ج"
+    },
+    {
+        "id": 3,
+        "text": "",
+        "options": [
+            {"label": "أ", "text": "معلمةُ", "value": "أ"},
+            {"label": "ب", "text": "معلمَهُ", "value": "ب"},
+            {"label": "ج", "text": " معلمهو", "value": "ج"},
+        ],
+        "correct": "د"
+    },
+
+
+
+    {
+        "id": 4,
+        "text": "",
+        "options": [
+            {"label": "أ", "text": "بعقلِ", "value": "أ"},
+            {"label": "ب", "text": "بعقلي", "value": "ب"},
+            {"label": "ج", "text": "بعقليا", "value": "ج"},
+        ],
+        "correct": "د"
+    },
+    {
+        "id": 5,
+        "text": "",
+        "options": [
+            {"label": "أ", "text": "مُتعاؤنون", "value": "أ"},
+            {"label": "ب", "text": "مُتعاونونَ", "value": "ب"},
+            {"label": "ج", "text": "متعاونُ", "value": "ج"},
+        ],
+        "correct": "أ"
+    },
+    {
+        "id": 6,
+        "text": "",
+        "options": [
+            {"label": "أ", "text": "ذَلِكَ", "value": "أ"},
+            {"label": "ب", "text": "ذالَك", "value": "ب"},
+            {"label": "ج", "text": "ذالِكا", "value": "ج"},
+        ],
+        "correct": "د"
+    },
+    {
+        "id": 7,
+        "text": "",
+        "options": [
+            {"label": "أ", "text": "دونيا", "value": "أ"},
+            {"label": "ب", "text": "دُنْيا", "value": "ب"},
+            {"label": "ج", "text": "دُنيي", "value": "ج"},
+        ],
+        "correct": "د"
+    },
+    {
+        "id": 8,
+        "text": "",
+        "options": [
+            {"label": "أ", "text": "المُستَقمُ", "value": "أ"},
+            {"label": "ب", "text": "المُستاقِمُ", "value": "ب"},
+            {"label": "ج", "text": "المُسْتقيمُ", "value": "ج"},
+        ],
+        "correct": "أ"
+    },
+    {
+        "id": 9,
+        "text": "",
+        "options": [
+            {"label": "أ", "text": "الضهرِ", "value": "أ"},
+            {"label": "ب", "text": "الظُّهرِ", "value": "ب"},
+            {"label": "ج", "text": "الذهرِ", "value": "ج"},
+        ],
+        "correct": "ج"
+    },
+    {
+        "id": 10,
+        "text": "",
+        "options": [
+            {"label": "أ", "text": "يُسافِرونا", "value": "أ"},
+            {"label": "ب", "text": "يُسافِرونَ", "value": "ب"},
+            {"label": "ج", "text": "يسافِرنَ", "value": "ج"},
+        ],
+        "correct": "أ"
+    },
+    {
+        "id": 11,
+        "text": "",
+        "options": [
+            {"label": "أ", "text": "هاذِهِ", "value": "أ"},
+            {"label": "ب", "text": "هاذِهي", "value": "ب"},
+            {"label": "ج", "text": "هذِهِ", "value": "ج"},
+        ],
+        "correct": "د"
+    },
+    {
+        "id": 12,
+        "text": "",
+        "options": [
+            {"label": "أ", "text": "سأفاسِرُ", "value": "أ"},
+            {"label": "ب", "text": "سأفارِسُ", "value": "ب"},
+            {"label": "ج", "text": "سأسافِرُ", "value": "ج"},
+        ],
+        "correct": "ب"
+    },
+    {
+        "id": 13,
+        "text": "",
+        "options": [
+            {"label": "أ", "text": "أيْظاً", "value": "أ"},
+            {"label": "ب", "text": "أيْضاً", "value": "ب"},
+            {"label": "ج", "text": "أيْضَنْ", "value": "ج"},
+        ],
+        "correct": "ج"
+    },
+    {
+        "id": 14,
+        "text": "",
+        "options": [
+            {"label": "أ", "text": "قَزالٌ", "value": "أ"},
+            {"label": "ب", "text": "غَذالٌ", "value": "ب"},
+            {"label": "ج", "text": "غَزالٌ", "value": "ج"},
+        ],
+        "correct": "ب"
+    },
+    {
+        "id": 15,
+        "text": "",
+        "options": [
+            {"label": "أ", "text": "الجُرْأَةُ", "value": "أ"},
+            {"label": "ب", "text": "الجُرْءَةُ", "value": "ب"},
+            {"label": "ج", "text": "الجُرْئةُ", "value": "ج"},
+        ],
+        "correct": "ب"
+    },
+    {
+        "id": 16,
+        "text": "",
+        "options": [
+            {"label": "أ", "text": "عادوا", "value": "أ"},
+            {"label": "ب", "text": "عادو", "value": "ب"},
+            {"label": "ج", "text": "عدوا", "value": "ج"},
+        ],
+        "correct": "أ"
+    },
+    {
+        "id": 17,
+        "text": "",
+        "options": [
+            {"label": "أ", "text": "لاكِنْ", "value": "أ"},
+            {"label": "ب", "text": "لكِنَّ", "value": "ب"},
+            {"label": "ج", "text": "لاكينْ", "value": "ج"},
+        ],
+        "correct": "ج"
+    },
+    {
+        "id": 18,
+        "text": "",
+        "options": [
+            {"label": "أ", "text": "ذهبْتو", "value": "أ"},
+            {"label": "ب", "text": "ذهبةُ", "value": "ب"},
+            {"label": "ج", "text": "ذهبْتُ", "value": "ج"},
+        ],
+        "correct": "ب"
+    },
+    {
+        "id": 19,
+        "text": "",
+        "options": [
+            {"label": "أ", "text": "للِشَّمالي", "value": "أ"},
+            {"label": "ب", "text": "للِشَّمالِ", "value": "ب"},
+            {"label": "ج", "text": "لِشَّمالِ", "value": "ج"},
+        ],
+        "correct": "ج"
+    },
+    {
+        "id": 20,
+        "text": "",
+        "options": [
+            {"label": "أ", "text": "المُأَذّنِ", "value": "أ"},
+            {"label": "ب", "text": "المُؤَذّنِ", "value": "ب"},
+            {"label": "ج", "text": "الموأَذّنِ", "value": "ج"},
+        ],
+        "correct": "ب"
+    },
+    {
+        "id": 21,
+        "text": "",
+        "options": [
+            {"label": "أ", "text": "الساهِرتُ", "value": "أ"},
+            {"label": "ب", "text": "الساهِرةُ", "value": "ب"},
+            {"label": "ج", "text": "السهرتو", "value": "ج"},
+        ],
+        "correct": "ج"
+    },
+    {
+        "id": 22,
+        "text": "",
+        "options": [
+            {"label": "أ", "text": "شَجَرتاً", "value": "أ"},
+            {"label": "ب", "text": "شَجَرتَنْ", "value": "ب"},
+            {"label": "ج", "text": "شَجَرةً", "value": "ج"},
+        ],
+        "correct": "ج"
+    },
+    {
+        "id": 23,
+        "text": "",
+        "options": [
+            {"label": "أ", "text": "سَئَكتُبُ", "value": "أ"},
+            {"label": "ب", "text": "سَاكْتُبُ", "value": "ب"},
+            {"label": "ج", "text": "سَأَكتُبُ", "value": "ج"},
+        ],
+        "correct": "د"
+    },
+    {
+        "id": 24,
+        "text": "",
+        "options": [
+            {"label": "أ", "text": "الذانِ", "value": "أ"},
+            {"label": "ب", "text": "اللذاني", "value": "ب"},
+            {"label": "ج", "text": "اللذانِ", "value": "ج"},
+        ],
+        "correct": "ب"
+    },
+    {
+        "id": 25,
+        "text": "",
+        "options": [
+            {"label": "أ", "text": "كَلوردِ", "value": "أ"},
+            {"label": "ب", "text": "كَلْ الوردِ", "value": "ب"},
+            {"label": "ج", "text": "كَالوردِ", "value": "ج"},
+        ],
+        "correct": "أ"
+    },
+    {
+        "id": 26,
+        "text": "",
+        "options": [
+            {"label": "أ", "text": "اسْتغالها", "value": "أ"},
+            {"label": "ب", "text": "اسْتغل ها", "value": "ب"},
+            {"label": "ج", "text": "اِسْتغلها", "value": "ج"},
+        ],
+        "correct": "د"
+    },
+    {
+        "id": 27,
+        "text": "",
+        "options": [
+            {"label": "أ", "text": "تفاأَلَ", "value": "أ"},
+            {"label": "ب", "text": "تفاءَلَ", "value": "ب"},
+            {"label": "ج", "text": "تَفَألَ", "value": "ج"},
+        ],
+        "correct": "ب"
+    },
+    {
+        "id": 28,
+        "text": "",
+        "options": [
+            {"label": "أ", "text": "التَّشؤمِ", "value": "أ"},
+            {"label": "ب", "text": "التَّشاأُمِ", "value": "ب"},
+            {"label": "ج", "text": "التَّشاؤمِ", "value": "ج"},
+        ],
+        "correct": "ج"
+    },
+    
+]
