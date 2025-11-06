18454288c4ba0219a70ff88da383eb83e8568584
smerri <smerri@kisr.edu.kw>
2025-11-06 10:31:44 +0300

    add audio for primary test 4

--- electronic_diagnosis_test/data/primary/test4.py
+++ electronic_diagnosis_test/data/primary/test4.py
@@ -1,11 +1,11 @@
-primary_test4_words = [
+'''primary_test4_words = [
     "", "", "", "", "",
     "", "", "", "", "",
     "", "", "","","",
     "", "", "", "", "",
     "", "", "", "","",
     "", "", "", "", ""
-]
+]'''
 
 '''primary_test4_training_words = [
         {
@@ -26,191 +26,153 @@ primary_test4_words = [
         #"audio_sentence": "Sample.mp3",
         #"audio_word": "RWE04.mp3"
     },
-]
+]'''
 
 primary_test4_words = [
     {
-        "text": "أنتم",
-        #"sentence": "وظيفة اللسان التذوق",
-        #"audio_sentence": "Main01.mp3",
-        #"audio_word": "RWE07.mp3"
+        "text": "هذه",
+        "audio_sentence": "هذه_1.mp3",
+        
     },
     {
-        "text": "كأننا",
-        ##"sentence": "وظيفة اللسان التذوق",
-        #"audio_sentence": "Main02.mp3",
-        #"audio_word": "RWE09.mp3"
+        "text": "الضوء",
+        "audio_sentence": "الضوء_2.mp3",
     },
     {
-        "text": "معلمه",
-        ##"sentence": "وظيفة اللسان التذوق",
-        #"audio_sentence": "Main03.mp3",
-        #"audio_word": "RWE011.mp3"
+        "text": "كراستك",
+        "audio_sentence": "كراستك_3.mp3",
     },
     {
-        "text": "بعقلي",
-        ##"sentence": "وظيفة اللسان التذوق",
-        #"audio_sentence": "Main04.mp3",
-        #"audio_word": "RWE0127.mp3"
+        "text": "فوائد",
+        "audio_sentence": "فوائد_4.mp3",
+
     },
     {
-        "text": "متعاونون",
-        ##"sentence": "وظيفة اللسان التذوق",
-        #"audio_sentence": "Main05.mp3",
-        #"audio_word": "RWE013.mp3"
+        "text": "الشتاء",
+        "audio_sentence": "الشتاء_5.mp3",
     },
     {
-        "text": "ذلك",
-        ##"sentence": "وظيفة اللسان التذوق",
-        #"audio_sentence": "Main06.mp3",
-        #"audio_word": "RWE017.mp3"
+        "text": "الصحيحة",
+        "audio_sentence": "الصحيحة_6.mp3",
+
     },
     {
-        "text": "دنيا",
-        ##"sentence": "وظيفة اللسان التذوق",
-        #"audio_sentence": "Main07.mp3",
-        #"audio_word": "RWE019.mp3"
+        "text": "الزاوية",
+        "audio_sentence": "الزاوية_7.mp3",
     },
     {
-        "text": "المستقيم",
-        #"sentence": "وظيفة اللسان التذوق",
-        #"audio_sentence": "Main08.mp3",
-        #"audio_word": "RWE0125.mp3"
+        "text": "اللغوية",
+        "audio_sentence": "اللغوية_8.mp3",
     },
         {
-        "text": "الظهر",
-        #"sentence": "وظيفة اللسان التذوق",
-        #"audio_sentence": "Main09.mp3",
-        #"audio_word": "RWE0145.mp3"
+        "text": "تنتمي",
+        "audio_sentence": "تنتمي_9.mp3",
     },
      
     {
-        "text": "يسافرون",
-        #"sentence": "وظيفة اللسان التذوق",
-        #"audio_sentence": "Main010.mp3",
-        #"audio_word": "RWE0129.mp3"
+        "text": "اللعب",
+        "audio_sentence": "اللعب_10.mp3",
     },
                 {
-        "text": "هذه",
-        #"sentence": "وظيفة اللسان التذوق",
-        #"audio_sentence": "Main011.mp3",
-        #"audio_word": "RWE0133.mp3"
+        "text": "التذوق",
+        "audio_sentence": "التذوق_11.mp3",
     },
         {
-        "text": "سأسافر",
-        #"sentence": "وظيفة اللسان التذوق",
-        #"audio_sentence": "Main012.mp3",
-        #"audio_word": "RWE0151.mp3"
+        "text": "هؤلاء",
+        "audio_sentence": "هؤلاء_12.mp3",
     },
 
                 {
-        "text": "أيضا",
-        #"sentence": "وظيفة اللسان التذوق",
-        #"audio_sentence": "Main013.mp3",
-        #"audio_word": "RWE0135.mp3"
+        "text": "الأبيات",
+        "audio_sentence": "الأبيات_13.mp3",
     },
 
             {
-        "text": "غزال",
-        #"sentence": "وظيفة اللسان التذوق",
-        #"audio_sentence": "Main014.mp3",
-        #"audio_word": "RWE0137.mp3"
+        "text": "المكانية",
+        "audio_sentence": "المكانية_14.mp3",
     },
 
             {
-        "text": "الجرأة",
-        ##"sentence": "وظيفة اللسان التذوق",
-        #"audio_sentence": "Main015.mp3",
-        #"audio_word": "RWE0123.mp3"
+        "text": "الصيدلية",
+        "audio_sentence": "الصيدلية_15.mp3",
     },
 
         {
-        "text": "عادوا",
-        ##"sentence": "وظيفة اللسان التذوق",
-        #"audio_sentence": "Main016.mp3",
-        #"audio_word": "RWE0121.mp3"
+        "text": "الكهربائية",
+        "audio_sentence": "الكهربائية_16.mp3",
     },
 
             {
-        "text": "لكن",
-        #"sentence": "وظيفة اللسان التذوق",
-        #"audio_sentence": "Main017.mp3",
-        #"audio_word": "RWE0139.mp3"
+        "text": "أخرى",
+        "audio_sentence": "أخرى_17.mp3",
     },
 
             {
-        "text": "ذهبت",
-        #"sentence": "وظيفة اللسان التذوق",
-        #"audio_sentence": "Main018.mp3",
-        #"audio_word": "RWE0141.mp3"
+        "text": "آلاف",
+        "audio_sentence": "آلاف_18.mp3",
     },
 
             {
-        "text": "للشمال",
-        #"sentence": "وظيفة اللسان التذوق",
-        #"audio_sentence": "Main019.mp3",
-        #"audio_word": "RWE0143.mp3"
+        "text": "كذلك",
+        "audio_sentence": "كذلك_19.mp3",
     },
 
             {
-        "text": "المؤذن",
-        #"sentence": "وظيفة اللسان التذوق",
-        #"audio_sentence": "Main020.mp3",
-        #"audio_word": "RWE0131.mp3"
+        "text": "الليالي",
+
+        "audio_sentence": "الليالي_20.mp3",
+
     },
 
     {
-        "text": "الساهرة",
-        #"sentence": "وظيفة اللسان التذوق",
-        #"audio_sentence": "Main021.mp3",
-        #"audio_word": "RWE0147.mp3"
+        "text": "المؤمن",
+        "audio_sentence": "المؤمن_21.mp3",
+
     },
 
     {
-        "text": "شجرة",
-        #"sentence": "وظيفة اللسان التذوق",
-        #"audio_sentence": "Main022.mp3",
-        #"audio_word": "RWE0149.mp3"
+        "text": "ترفيهية",
+        "audio_sentence": "ترفيهية_22.mp3",
     },
 
 
 
 
             {
-        "text": "سأكتب",
-        #"sentence": "وظيفة اللسان التذوق",
-        #"audio_sentence": "Main023.mp3",
-        #"audio_word": "RWE0153.mp3"
+        "text": "السائلة",
+        "audio_sentence": "السائلة_23.mp3",
     },
         {
-        "text": "اللذان",
-        #"sentence": "وظيفة اللسان التذوق",
-        #"audio_sentence": "Main024.mp3",
-        #"audio_word": "RWE0155.mp3"
+        "text": "الأولى",
+        "audio_sentence": "الأولى_24.mp3",
     },
     {
-        "text": "كالورد",
-        #"sentence": "وظيفة اللسان التذوق",
-        #"audio_sentence": "Main025.mp3",
-        #"audio_word": "RWE0157.mp3"
+        "text": "زملائك",
+        "audio_sentence": "زملائك_25.mp3",
     },
     {
-        "text": "استغلها",
-        #"sentence": "وظيفة اللسان التذوق",
-        #"audio_sentence": "Main026.mp3",
-        #"audio_word": "RWE0159.mp3"
+        "text": "الضوئية",
+        "audio_sentence": "الضوئية_26.mp3",
+
     },
     {
-        "text": "تفاءل",
-        #"sentence": "وظيفة اللسان التذوق",
-        #"audio_sentence": "Main027.mp3",
-        #"audio_word": "RWE0161.mp3"
+        "text": "الآخرة",
+        "audio_sentence": "الآخرة_27.mp3",
     },
     {
-        "text": "التشاؤم",
-        #"sentence": "وظيفة اللسان التذوق",
-        #"audio_sentence": "Main028.mp3",
-        #"audio_word": "RWE0163.mp3"
+        "text": "كفروا",
+        "audio_sentence": "كفروا_28.mp3",
+
+    },
+        {
+        "text": "يؤدي",
+        "audio_sentence": "يؤدي_29.mp3",
+
+    },
+            {
+        "text": "الإضاءة",
+        "audio_sentence": "الإضاءة_30.mp3",
+
     },
 
 
@@ -229,4 +191,4 @@ primary_test4_words = [
 
 
 
-]'''
+]
